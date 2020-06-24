ed317b15a61052c22857b55cf72a30da8e3ddf27
GKAtwood <gabrielle.k85@gmail.com>
2020-06-24 15:21:12 -0600

    sdfs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      dropdownView: false
+    }
+  }
+
+  toggleDropdown =()=>{
+    this.setState({dropdownView: !this.state.dropdownView})
+  }
+
+render(){
+  return(
+        <div className ="App">
+            <header className = 'main-header'>
+              <h1>Start Bootstrap</h1>
+              <nav className= 'desktop-menu'>
+                <span>SERVICES</span>
+                <span>PORTFOLIO</span>
+                <span>ABOUT</span>
+                <span>TEAM</span>
+                <span>CONTACT</span>
+              </nav>
+              <div className = 'dropdown' onClick = {this.toggleDropdown}>MENU</div>
+                {this.state.dropdownView
+                ? (
+                <nav className='mobile-menu'>
+                  <span>SERVICES</span>
+                  <span>PORTFOLIO</span>
+                  <span>ABOUT</span>
+                  <span>TEAM</span>
+                  <span>CONTACT</span>
+                </nav>
+                )
+              : null}
+          </header>
+
+          <section className='section'>
+                  <h2>Welcome to our studio!</h2>
+                  <h3>IT'S NICE TO MEET YOU</h3>
+                  <button>TELL ME MORE</button>
+
+
+          </section>
+
+
+  </div>
+
+     )
+  }
+
 }
+
+
 
 export default App;
